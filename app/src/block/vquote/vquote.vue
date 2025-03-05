822944c8e59b11f0b99faf93928f60c14437de9e
Andrey Vorozhko <vorojan@mail.ru>
2025-03-05 15:31:04 +0400

    fix: добавлена очистка всех тегов кроме разрешенных в поля редактора vue. Т-80474

--- app/src/block/vquote/vquote.vue
+++ app/src/block/vquote/vquote.vue
@@ -1,16 +1,27 @@
 <template>
-  <vue-editor v-model="editorData" :editorToolbar="customToolbar"></vue-editor>
+  <vue-editor v-model="editorData" :editorToolbar="customToolbar" :editor-options="editorSettings"></vue-editor>
 </template>
 
 <script>
     import { VueEditor } from "vue2-editor";
+
     export default {
       components:{VueEditor},
       name: "vquote",
       data(){
         return {
           editorData:"",
-          customToolbar: [["bold", "italic", "underline"]]
+          customToolbar: [["bold", "italic", "underline"]],
+          editorSettings: {
+            modules: {
+              cleanTags: {
+                allowedAttributes: ["italic", "bold", "underline"],
+              },
+              clipboard: {
+                matchVisual: false
+              }
+            }
+          }
         }
       }
     }
